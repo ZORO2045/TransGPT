2460ce6859088187edd654d93417d28cd1c9af03
ፚ Ꭷ Ꮢ Ꭷ ❥ <101139382+ZORO2045@users.noreply.github.com>
2024-05-16 02:59:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,30 @@
 const dropdowns = document.querySelectorAll(".dropdown-container"),
   inputLanguageDropdown = document.querySelector("#input-language"),
-  outputLanguageDropdown = document.querySelector("#output-language"),
-  inputTextElem = document.querySelector("#input-text"),
-  outputTextElem = document.querySelector("#output-text"),
-  inputChars = document.querySelector("#input-chars"),
-  swapBtn = document.querySelector(".swap-position"),
-  darkModeCheckbox = document.getElementById("dark-mode-btn"),
-  uploadDocument = document.querySelector("#upload-document"),
-  downloadBtn = document.querySelector("#download-btn");
+  outputLanguageDropdown = document.querySelector("#output-language");
 
 function populateDropdown(dropdown, options) {
-  const ul = dropdown.querySelector("ul");
-  ul.innerHTML = "";
+  dropdown.querySelector("ul").innerHTML = "";
   options.forEach((option) => {
     const li = document.createElement("li");
-    const title = `${option.name} (${option.native})`;
+    const title = option.name + " (" + option.native + ")";
     li.innerHTML = title;
     li.dataset.value = option.code;
     li.classList.add("option");
-    ul.appendChild(li);
+    dropdown.querySelector("ul").appendChild(li);
   });
 }
 
-function toggleDropdown(dropdown) {
-  dropdown.classList.toggle("active");
-}
-
-function switchLanguages() {
-  const temp = inputLanguageDropdown.innerHTML;
-  inputLanguageDropdown.innerHTML = outputLanguageDropdown.innerHTML;
-  outputLanguageDropdown.innerHTML = temp;
-
-  const tempValue = inputLanguageDropdown.dataset.value;
-  inputLanguageDropdown.dataset.value = outputLanguageDropdown.dataset.value;
-  outputLanguageDropdown.dataset.value = tempValue;
-
-  const tempInputText = inputTextElem.value;
-  inputTextElem.value = outputTextElem.value;
-  outputTextElem.value = tempInputText;
-
-  translate();
-}
-
-function translate() {
-  const inputText = inputTextElem.value;
-  const inputLanguage = inputLanguageDropdown.dataset.value;
-  const outputLanguage = outputLanguageDropdown.dataset.value;
-  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(inputText)}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => {
-      outputTextElem.value = json[0].map((item) => item[0]).join("");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-function handleInputChange() {
-  if (inputTextElem.value.length > 5000) {
-    inputTextElem.value = inputTextElem.value.slice(0, 5000);
-  }
-  translate();
-  inputChars.innerHTML = inputTextElem.value.length;
-}
-
-function handleFileUpload(e) {
-  const file = e.target.files[0];
-  if (
-    file.type === "application/pdf" ||
-    file.type === "text/plain" ||
-    file.type === "application/msword" ||
-    file.type === "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-  ) {
-    uploadTitle.innerHTML = file.name;
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = (e) => {
-      inputTextElem.value = e.target.result;
-      translate();
-    };
-  } else {
-    alert("Please upload a valid file");
-  }
-}
-
-function handleDownload() {
-  const outputText = outputTextElem.value;
-  const outputLanguage = outputLanguageDropdown.dataset.value;
-  if (outputText) {
-    const blob = new Blob([outputText], { type: "text/plain" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.download = `translated-to-${outputLanguage}.txt`;
-    a.href = url;
-    a.click();
-  }
-}
-
-function toggleDarkMode() {
-  document.body.classList.toggle("dark");
-}
+populateDropdown(inputLanguageDropdown, languages);
+populateDropdown(outputLanguageDropdown, languages);
 
 dropdowns.forEach((dropdown) => {
   dropdown.addEventListener("click", (e) => {
-    toggleDropdown(dropdown);
+    dropdown.classList.toggle("active");
   });
 
   dropdown.querySelectorAll(".option").forEach((item) => {
     item.addEventListener("click", (e) => {
+      //remove active class from current dropdowns
       dropdown.querySelectorAll(".option").forEach((item) => {
         item.classList.remove("active");
       });
@@ -120,7 +36,6 @@ dropdowns.forEach((dropdown) => {
     });
   });
 });
-
 document.addEventListener("click", (e) => {
   dropdowns.forEach((dropdown) => {
     if (!dropdown.contains(e.target)) {
@@ -129,22 +44,104 @@ document.addEventListener("click", (e) => {
   });
 });
 
+const swapBtn = document.querySelector(".swap-position"),
+  inputLanguage = inputLanguageDropdown.querySelector(".selected"),
+  outputLanguage = outputLanguageDropdown.querySelector(".selected"),
+  inputTextElem = document.querySelector("#input-text"),
+  outputTextElem = document.querySelector("#output-text");
+
 swapBtn.addEventListener("click", (e) => {
-  switchLanguages();
+  const temp = inputLanguage.innerHTML;
+  inputLanguage.innerHTML = outputLanguage.innerHTML;
+  outputLanguage.innerHTML = temp;
+
+  const tempValue = inputLanguage.dataset.value;
+  inputLanguage.dataset.value = outputLanguage.dataset.value;
+  outputLanguage.dataset.value = tempValue;
+
+  //swap text
+  const tempInputText = inputTextElem.value;
+  inputTextElem.value = outputTextElem.value;
+  outputTextElem.value = tempInputText;
+
+  translate();
 });
 
-darkModeCheckbox.addEventListener("change", () => {
-  toggleDarkMode();
-});
-
+function translate() {
+  const inputText = inputTextElem.value;
+  const inputLanguage =
+    inputLanguageDropdown.querySelector(".selected").dataset.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(
+    inputText
+  )}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      outputTextElem.value = json[0].map((item) => item[0]).join("");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 inputTextElem.addEventListener("input", (e) => {
-  handleInputChange();
+  //limit input to 5000 characters
+  if (inputTextElem.value.length > 5000) {
+    inputTextElem.value = inputTextElem.value.slice(0, 5000);
+  }
+  translate();
 });
+
+const uploadDocument = document.querySelector("#upload-document"),
+  uploadTitle = document.querySelector("#upload-title");
 
 uploadDocument.addEventListener("change", (e) => {
-  handleFileUpload(e);
+  const file = e.target.files[0];
+  if (
+    file.type === "application/pdf" ||
+    file.type === "text/plain" ||
+    file.type === "application/msword" ||
+    file.type ===
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  ) {
+    uploadTitle.innerHTML = file.name;
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      inputTextElem.value = e.target.result;
+      translate();
+    };
+  } else {
+    alert("Please upload a valid file");
+  }
 });
 
+const downloadBtn = document.querySelector("#download-btn");
+
 downloadBtn.addEventListener("click", (e) => {
-  handleDownload();
+  const outputText = outputTextElem.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  if (outputText) {
+    const blob = new Blob([outputText], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.download = `translated-to-${outputLanguage}.txt`;
+    a.href = url;
+    a.click();
+  }
+});
+
+const darkModeCheckbox = document.getElementById("dark-mode-btn");
+
+darkModeCheckbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
+
+const inputChars = document.querySelector("#input-chars");
+
+inputTextElem.addEventListener("input", (e) => {
+  inputChars.innerHTML = inputTextElem.value.length;
 });
